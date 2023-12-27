4027b736c7692a410e7218f34396932c7c6c6461
Darshan Parameshwar Moger <130597128+drsnpm@users.noreply.github.com>
2023-12-27 19:19:52 +0530

    Update nav7.py

--- nav7.py
+++ nav7.py
@@ -1,17 +1,23 @@
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn import datasets
 import pandas as pd
 import numpy as np
-iris=datasets.load_iris()
-x=pd.DataFrame(iris.data)
-x.columns=['sepal_length','sepal_width','petal_length','petal_width']
-y=pd.DataFrame(iris.target)
-y.columns=["Targets"]
-model=KMeans(n_clusters=3)
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+from sklearn import datasets
+iris = datasets.load_iris()
+x = pd.DataFrame(iris.data)
+x.columns = ['sepal_length','sepal_width','petal_length','petal_width']
+y = pd.DataFrame(iris.target)
+y.columns = ["Targets"]
+model =  KMeans(n_clusters=3)
 model.fit(x)
+colormap = np.array(['red','lime','black'])
 plt.figure(figsize=(14,14))
-colormap=np.array(['red','lime','black'])
+xs = pd.DataFrame(x)
+gmm=GaussianMixture(n_components=3)
+gmm.fit(xs)
+gmm_y=gmm.predict(xs)
+
 plt.subplot(2,2,1)
 plt.scatter(x.petal_length,x.petal_width,c=colormap[y.Targets],s=40)
 plt.title("real custer")
@@ -22,16 +28,6 @@ plt.scatter(x.petal_length,x.petal_width,c=colormap[model.labels_],s=40)
 plt.title("kmeans custer")
 plt.xlabel("petal_length")
 plt.ylabel("petal_width")
-
-from sklearn import preprocessing
-scaler=preprocessing.StandardScaler()
-scaler.fit(x)
-xsa=scaler.transform(x)
-xs=pd.DataFrame(xsa,columns=x.columns)
-from sklearn.mixture import GaussianMixture
-gmm=GaussianMixture(n_components=3)
-gmm.fit(xs)
-gmm_y=gmm.predict(xs)
 plt.subplot(2,2,3)
 plt.scatter(x.petal_length,x.petal_width,c=colormap[gmm_y],s=40)
 plt.title("gmm custer")
